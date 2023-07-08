4694ac459e2a53f250b1cf951f45929ea4134878
unknown <khobragade.shubham@gmail.com>
2023-07-08 14:31:36 +0530

    updated code

---
+++ src/components/Covid.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import './Covidd.css';
+
+const Covid = () => {
+    
+    return (
+        <>
+        
+            <h1>LIVE</h1>
+            <h2>COVID-19 CORONAVIRUS TRACKER</h2>
+            <section>
+            <ul>
+                <li className="card">
+                    <div className="card_main">
+                        <div className="card_inner">
+                            <p className="card_name"><span> OUR </span> COUNTRY</p>
+                            <p className="card_total card_small">INDIA</p>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+            <li className="card">
+                    <div className="card_main">
+                        <div className="card_inner">
+                            <p className="card_name"><span> OUR </span> COUNTRY</p>
+                            <p className="card_total card_small">INDIA</p>
+                        </div>
+                    </div>
+                </li>
+                <li className="card">
+                    <div className="card_main">
+                        <div className="card_inner">
+                            <p className="card_name"><span> OUR </span> COUNTRY</p>
+                            <p className="card_total card_small">INDIA</p>
+                        </div>
+                    </div>
+                </li>
+                <li className="card">
+                    <div className="card_main">
+                        <div className="card_inner">
+                            <p className="card_name"><span> OUR </span> COUNTRY</p>
+                            <p className="card_total card_small">INDIA</p>
+                        </div>
+                    </div>
+                </li>
+                <li className="card">
+                    <div className="card_main">
+                        <div className="card_inner">
+                            <p className="card_name"><span> OUR </span> COUNTRY</p>
+                            <p className="card_total card_small">INDIA</p>
+                        </div>
+                    </div>
+                </li>
+                <li className="card">
+                    <div className="card_main">
+                        <div className="card_inner">
+                            <p className="card_name"><span> OUR </span> COUNTRY</p>
+                            <p className="card_total card_small">INDIA</p>
+                        </div>
+                    </div>
+                </li>
+            
+            </section>
+        </>
+    )
+}
+
+export default Covid
